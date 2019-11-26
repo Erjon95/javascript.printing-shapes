7f0c22467e31c50755b92246ec602eb8d92fe72b
git-leon <xleonhunter@gmail.com>
2019-11-25 21:37:40 -0500

    refactored project to enforce `TDD` practices

--- assets/js/shapes.js
+++ assets/js/shapes.js
@@ -1,66 +1,31 @@
 function getLine(length) {
-    line = "";
-    for(let i=0; i<length; i++) {
-        line += "*";
-    }
-    return line;
+    // TODO - write method definition here
 }
 
 
 
 function getBox(width, height) {
-    output = "";
-    for(let i=0; i<height; i++) {
-        output += getLine(width) + "\n";
-    }
-    return output;
+    // TODO - write method definition here
 }
 
 
 
 function getBottomLeftTriangle(length) {
-    result = "";
-    for(let j=1; j<=length; j++) {
-        result += getLine(j) + "\n";
-    }
-    return result;
+    // TODO - write method definition here
 }
 
 
 function getUpperLeftTriangle(length) {
-    result = "";
-    for(let i=length; i>=0; i--) {
-        result += getLine(i) + "\n";
-    }
-    return result;
+    // TODO - write method definition here
 }
 
 
 
 function getPyramid(length) {
-    let result = "";
-    for(let i=1; i<= length; i++){
-        let str = ' '.repeat(length-i);
-        let str2 = '*'. repeat(i*2 -1)
-
-        result += str + str2 + str + "\n";
-    }
-    return result;
+    // TODO - write method definition here
 }
 
 
 function getCheckerboard(width, height) {
-    var board = "";
-
-    for (var y = 0; y < height; y++) {
-        for (var x = 0; x < width; x++) {
-            if ((x + y) % 2 == 0) {
-                board += " ";
-            } else {
-                board += "*";
-            }
-        }
-        board += "\n";        
-    }
-    return board;
+    // TODO - write method definition here
 }
